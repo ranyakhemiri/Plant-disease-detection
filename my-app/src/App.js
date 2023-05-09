a68d7e7955af03df0bb707d5b048e5d43ff2281e
Rania Khemiri <rkhemiri@amazon.fr>
2023-05-09 12:45:35 +0200

    added upload button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './aws_logo.svg';
 import './App.css';
 
 function App() {
@@ -6,17 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <input className= "upload" type='file' accept='image/*'/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Upload your plant picture here.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
