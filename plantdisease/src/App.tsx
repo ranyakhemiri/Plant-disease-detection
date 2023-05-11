63320c7fb081da97585d500a291bf8003e7f1628
Rania Khemiri <rkhemiri@amazon.fr>
2023-05-11 17:17:37 +0100

    private upload to s3

--- plantdisease/src/App.tsx
+++ plantdisease/src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { FileUploader, withAuthenticator} from '@aws-amplify/ui-react';
 import "@aws-amplify/ui-react/styles.css";
 
 function App() {
   return (
     <FileUploader
-      accessLevel='public'
+      accessLevel='private'
       acceptedFileTypes={
         ['image/*']
       }
